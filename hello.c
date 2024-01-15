2c1945f07c78b2b129c6c0601e36da0320f4db49
lingxu <lx659995.163.com>
2024-01-15 11:08:32 +0800

    test

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-<<<<<<< HEAD
 #include"world.h"
 
-=======
-brach hello-dev
->>>>>>> 0129c4ebd064993e5f74d8605c3693df43c23139
 int main() {
     printf("Hello ");
     show_world();
